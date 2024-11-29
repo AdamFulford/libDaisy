c77fced02eb6ad5b12f6daeefafe5b3092a7481c
AdamFulford <fulford.adam@gmail.com>
2024-11-29 22:16:40 +0000

    new board files

---
+++ src/daisy_VENOLOOPER_v5.cpp
@@ -0,0 +1,290 @@
+#include "daisy_VENOLOOPER_v5.h"
+#ifndef SAMPLE_RATE
+#define SAMPLE_RATE DSY_AUDIO_SAMPLE_RATE /**< & */
+#endif
+
+//v5.1 pin assignments
+//fix with seed assignments
+#define PIN_EOC2 D9
+#define PIN_EOC1 D8
+
+#define PIN_ENC_DOWN D10
+#define PIN_MIDI_UART_RX D14
+#define PIN_PLAY2_GATE D7
+#define PIN_UART_RP2040_TO_STM D11
+#define PIN_UART_STM_TO_RP2040 D12
+#define PIN_REC2_GATE D30
+
+#define PIN_MUX1_ADC A1
+#define PIN_ENC_UP D19
+#define PIN_Voct_CV2_ADC A5
+#define PIN_VOct_CV1_ADC A3
+#define PIN_LAYER_CV1_ADC A2
+#define PIN_LAYER_CV2_ADC A6
+#define PIN_START_CV1_ADC A0
+#define PIN_START_CV2_ADC A8
+#define PIN_MUX3_ADC A7
+#define PIN_MUX2_ADC A12
+
+#define PIN_MUX_SEL3 D32
+#define PIN_MUX_SEL1 D0
+#define PIN_MUX_SEL2 D27
+
+#define PIN_REC1_GATE D25
+#define PIN_CLOCK D26
+#define PIN_PLAY1_GATE D28
+
+#define PIN_SDMMC_SK D6
+#define PIN_SDMMC_CMD D5
+#define PIN_SDMMC_D0 D4
+#define PIN_SDMMC_D1 D3
+#define PIN_SDMMC_D2 D2
+#define PIN_SDMMC_D3 D1
+
+using namespace daisy;
+
+static constexpr I2CHandle::Config i2c_config
+    = {I2CHandle::Config::Peripheral::I2C_1,
+       {{DSY_GPIOB, 8}, {DSY_GPIOB, 9}},
+       I2CHandle::Config::Speed::I2C_1MHZ};
+
+static LedDriverPca9685<4, true>::DmaBuffer DMA_BUFFER_MEM_SECTION
+    led_dma_buffer_a,
+    led_dma_buffer_b;
+
+void VenoLooper_v5::Init(bool boost)
+{
+    seed.Init(boost);
+    seed.SetAudioBlockSize(256);
+    seed.SetAudioSampleRate(SaiHandle::Config::SampleRate::SAI_48KHZ);
+
+    dsy_gpio_pin CVpins[] = 
+    {seed::PIN_LENGTH_CV2_ADC,
+    seed::PIN_VOct_CV1_ADC,
+    seed::PIN_Voct_CV2_ADC,
+    seed::PIN_LAYER_CV1_ADC,
+    seed::PIN_LAYER_CV2_ADC,
+    seed::PIN_START_CV1_ADC,
+    seed::PIN_START_CV2_ADC,
+    seed::PIN_LENGTH_CV1_ADC};
+
+    //ADCs
+    AdcChannelConfig adc_cfg[LAST_CV + 2];
+
+    //For some reason muxes only work on channels below 8?
+    //mux1
+    adc_cfg[0].InitMux(seed::PIN_MUX1_ADC, 
+                             8,
+                             seed::PIN_MUX_SEL1,
+                             seed::PIN_MUX_SEL2,
+                             seed::PIN_MUX_SEL3,
+                             AdcChannelConfig::ConversionSpeed::SPEED_387CYCLES_5);
+
+    //mux2
+    adc_cfg[1].InitMux(seed::PIN_MUX2_ADC, 
+                             8,
+                             seed::PIN_MUX_SEL1,
+                             seed::PIN_MUX_SEL2,
+                             seed::PIN_MUX_SEL3,
+                             AdcChannelConfig::ConversionSpeed::SPEED_387CYCLES_5);  
+
+        //mux3
+    adc_cfg[2].InitMux(seed::PIN_MUX3_ADC, 
+                             8,
+                             seed::PIN_MUX_SEL1,
+                             seed::PIN_MUX_SEL2,
+                             seed::PIN_MUX_SEL3,
+                             AdcChannelConfig::ConversionSpeed::SPEED_387CYCLES_5);  
+
+    for(size_t i = 0; i < (LAST_CV); i++)
+    {
+        adc_cfg[i+3].InitSingle(CVpins[i]);
+    }    
+
+    seed.adc.Init(adc_cfg,11);
+
+    float CVUpdateFreq{};
+     //init CV handling
+    for(size_t i = 0; i < LAST_CV; i++)
+    {
+        CVUpdateFreq = CVFreq[i] == Slow ? AudioCallbackRate() : AudioCallbackRate() * AudioBlockSize();
+        cv[i].InitBipolarCv(seed.adc.GetPtr(i+2), CVUpdateFreq,CVSlew[i]);
+    }
+
+    uint8_t potIndex{};
+    float potUpdateFreq{};
+
+    //init pot handling
+     for(size_t i = 0; i < LAST_POT; i++)
+     {
+        potIndex = i < 8 ? i : i-8;
+        potUpdateFreq = PotFreq[i] == Slow ? AudioCallbackRate() : AudioCallbackRate() * AudioBlockSize();
+        pots[i].Init(seed.adc.GetMuxPtr(potIndex == i ? 0 : 1,potIndex),potUpdateFreq,false,false,PotSlew[i]);
+     }
+
+
+    //Gates
+    gates[0].Init(seed::PIN_PLAY1_GATE);
+    gates[1].Init(seed::PIN_REV1_GATE);
+    gates[2].Init(seed::PIN_REC1_GATE);
+
+    gates[3].Init(seed::PIN_PLAY2_GATE);
+    gates[4].Init(seed::PIN_REV2_GATE);
+    gates[5].Init(seed::PIN_REC2_GATE);
+
+    gates[6].Init(seed::PIN_CLOCK);
+
+    //Encoder
+    //SD Card
+    //MIDI
+    MidiUartHandler::Config midi_config;
+    midi_config.transport_config.periph = UartHandler::Config::Peripheral::USART_1;
+    midi_config.transport_config.rx = seed::PIN_MIDI_UART_RX;
+    midi_config.transport_config.tx = seed::PIN_MIDI_UART_TX;
+    midi.Init(midi_config);
+    midi.StartReceive();
+}
+
+void VenoLooper_v5::DelayMs(size_t del)
+{
+    seed.DelayMs(del);
+}
+
+void VenoLooper_v5::StartAudio(AudioHandle::InterleavingAudioCallback cb)
+{
+    seed.StartAudio(cb);
+}
+
+void VenoLooper_v5::StartAudio(AudioHandle::AudioCallback cb)
+{
+    seed.StartAudio(cb);
+}
+
+void VenoLooper_v5::StopAudio()
+{
+    seed.StopAudio();
+}
+
+void VenoLooper_v5::ChangeAudioCallback(AudioHandle::InterleavingAudioCallback cb)
+{
+    seed.ChangeAudioCallback(cb);
+}
+
+void VenoLooper_v5::ChangeAudioCallback(AudioHandle::AudioCallback cb)
+{
+    seed.ChangeAudioCallback(cb);
+}
+
+void VenoLooper_v5::SetHidUpdateRates()
+{
+    //set the hids to the new update rate
+    for(size_t i = 0; i < LAST_POT; i++)
+    {
+        pots[i].SetSampleRate(AudioCallbackRate());
+    }
+    for(size_t i = 0; i < LAST_CV; i++)
+    {
+        cv[i].SetSampleRate(AudioCallbackRate());
+    }
+}
+
+void VenoLooper_v5::SetAudioSampleRate(SaiHandle::Config::SampleRate samplerate)
+{
+    seed.SetAudioSampleRate(samplerate);
+    SetHidUpdateRates();
+}
+
+float VenoLooper_v5::AudioSampleRate()
+{
+    return seed.AudioSampleRate();
+}
+
+void VenoLooper_v5::SetAudioBlockSize(size_t size)
+{
+    seed.SetAudioBlockSize(size);
+    SetHidUpdateRates();
+}
+
+size_t VenoLooper_v5::AudioBlockSize()
+{
+    return seed.AudioBlockSize();
+}
+
+float VenoLooper_v5::AudioCallbackRate()
+{
+    return seed.AudioCallbackRate();
+}
+
+void VenoLooper_v5::StartAdc()
+{
+    seed.adc.Start();
+}
+
+void VenoLooper_v5::StopAdc()
+{
+    seed.adc.Stop();
+}
+
+void VenoLooper_v5::ProcessAnalogControls(VenoLooper_v5::CntrlFreq freq)
+{
+    for(size_t i = 0; i < LAST_POT; i++)
+    {
+        if(PotFreq[i]==freq)
+            pots[i].Process();
+    }
+
+
+    for(size_t i = 0; i < LAST_CV; i++)
+    {
+        if(CVFreq[i]==freq)
+            cv[i].Process();
+    }
+
+}
+
+void VenoLooper_v5::ProcessGates()
+{
+    for (size_t i = 0; i < LAST_GATE; i++)
+    {
+        gates[i].Update();
+    }
+        
+}
+
+void VenoLooper_v5::ProcessMCP23017()
+{
+    mcp.Read();
+}
+
+void VenoLooper_v5::DebounceMCP23017()
+{
+    for(size_t i=0; i < LAST_DIN; i++)
+    {
+        mcp.Debounce(i);
+    }
+}
+
+float VenoLooper_v5::GetKnobValue(Pots idx)
+{
+    return pots[idx < LAST_POT ? idx : 0].Value();
+}
+
+float VenoLooper_v5::GetCvValue(CVs idx)
+{
+    return cv[idx < LAST_CV ? idx : 0].Value();
+}
+
+bool VenoLooper_v5::GetRawPinState(DigitalInputs idx)
+{
+    return (mcp.GetPin(idx) == 0xFF) ? true : false;
+}
+
+AnalogControl* VenoLooper_v5::GetKnob(size_t idx)
+{
+    return &pots[idx < LAST_POT ? idx : 0];
+}
+
+AnalogControl* VenoLooper_v5::GetCv(size_t idx)
+{
+    return &cv[idx < LAST_CV ? idx : 0];
+}
---
+++ src/daisy_VENOLOOPER_v5.h
@@ -0,0 +1,342 @@
+#pragma once
+#ifndef VENO_LOOPER_V5_BOARD_H
+#define VENO_LOOPER_V5_BOARD_H /**< & */
+#include "daisy_seed.h"
+
+namespace daisy
+{
+
+class VenoLooper_v5
+{
+    public:
+
+    enum NeoPixel_LEDs //enum for addressing NeoPixels
+    {
+        LAST_NP_LED
+    };
+
+    enum Button_LEDs //enum for addressing button LEDs
+    {
+        LAST_BUTTON_LED
+    };
+
+
+
+    enum MUX_Inputs
+    {
+        //mux1
+        LENGTH1_POT,   //000
+        SPEED2_POT, //001
+        START1_POT, // 010
+        LENGTH2_POT,  //011
+        XFADE_POT, //100
+        PLAY_TOGGLE2_POT, //101
+        DECAY_POT, //110
+        PLAY_TOGGLE1_POT, //111
+
+        //mux2
+        ATTACK1_POT,
+        START2_POT,
+        SPEED1_POT,
+        ATTACK2_POT,
+        LAYER1_POT,
+        RELEASE2_POT,
+        LAYER2_POT,
+        RELEASE1_POT,
+
+        //mux3
+        INPUT1_CV,
+        LENGTH1_CV,
+        LENGTH2_CV,
+        MUX3_3,
+        INPUT2_CV,
+        MUX3_5,
+        MUX3_6,
+        CRUSH_POT,
+
+        LAST_POT
+    };
+
+    enum speedControls
+    {
+        SPEED1_POT_CTRL,
+        SPEED2_POT_CTRL,
+
+        SPEED1_CV_CTRL,
+        SPEED2_CV_CTRL,
+
+        LAST_SPEED    
+    };
+
+    enum PICO_Inputs
+    {
+        REC1_BUTTON,
+        PLAY1_BUTTON,
+        REV1_BUTTON,
+        STOP1_BUTTON,
+        CLR1_BUTTON,
+
+        REC2_BUTTON,
+        PLAY2_BUTTON,
+        REV2_BUTTON,
+        STOP2_BUTTON,
+        CLR2_BUTTON,
+
+        LINK_SWITCH,
+        QUANTISE_SWITCH,
+        SAVE_BUTTON,
+        ENC_CLICK,
+
+        REV1_GATE,
+        REV2_GATE,
+
+        LAST_PICO_INPUT    
+    };
+
+
+    enum CVs
+    {   
+        LENGTH2_CV,
+        VOct2_CV,
+        VOct1_CV,
+        LAYER1_CV,
+        LAYER2_CV,
+        START1_CV,
+        START2_CV,
+        LENGTH1_CV,
+        
+        LAST_CV
+    };
+
+    enum Daisy_Gates
+    {
+        PLAY1_GATE,
+        REC1_GATE,
+        PLAY2_GATE,
+        REC2_GATE,
+        CLOCK_GATE,
+
+        LAST_GATE
+    };
+
+    enum CntrlFreq
+    {Fast,Slow};
+
+    //pot param slew
+
+float PotSlew[LAST_POT] =
+{
+    0.05f, //XFADE_POT,   //000
+    0.05f, //PLAY_TOGGLE1_POT
+    0.2f, //SPEED1_POT
+    0.05f, //ATTACK1_POT
+    0.05f, //START1_POT
+    0.05f, //OVERDUB_DECAY_POT
+    0.000001f, //LAYER1_POT
+    0.05f, //LENGTH1_POT
+    0.05f, //LENGTH2_POT
+    0.000001f, //LAYER2_POT
+    0.05f, //START2_POT
+    0.05f, //RELEASE2_POT
+    0.2f, //SPEED2_POT
+    0.05f, //PLAY_TOGGLE2_POT
+    0.05f, //RELEASE1_POT
+    0.05f //ATTACK2_POT
+};
+
+float CVSlew[LAST_CV] =
+{
+    0.002f, //LENGTH2_CV
+    0.002f, //VOct2_CV
+    0.002f, //VOct1_CV
+    0.000001f, //LAYER1_CV
+    0.000001f, //LAYER2_CV
+    0.002f, //START1_CV
+    0.002f, //START2_CV
+    0.002f //LENGTH1_CV
+};
+
+CntrlFreq PotFreq[LAST_POT]
+{
+    Slow, //XFADE_POT,   //000
+    Slow, //PLAY_TOGGLE1_POT
+    Fast, //SPEED1_POT
+    Slow, //ATTACK1_POT
+    Slow, //START1_POT
+    Slow, //OVERDUB_DECAY_POT
+    Slow, //LAYER1_POT
+    Slow, //LENGTH1_POT
+    Slow, //LENGTH2_POT
+    Slow, //LAYER2_POT
+    Slow, //START2_POT
+    Slow, //RELEASE2_POT
+    Fast, //SPEED2_POT
+    Slow, //PLAY_TOGGLE2_POT
+    Slow, //RELEASE1_POT
+    Slow //ATTACK2_POT
+};
+
+CntrlFreq CVFreq[LAST_CV]
+{
+    Slow, //LENGTH2_CV
+    Fast, //VOct2_CV
+    Fast, //VOct1_CV
+    Slow, //LAYER1_CV
+    Slow, //LAYER2_CV
+    Slow, //START1_CV
+    Slow, //START2_CV
+    Slow //LENGTH1_CV
+};
+
+
+VenoLooper_v5() {}
+~VenoLooper_v5() {}
+
+void Init(bool boost = false);
+
+    /** 
+    Wait some ms before going on.
+    \param del Delay time in ms.
+    */
+    void DelayMs(size_t del);
+
+ /** Starts the callback
+    \param cb Interleaved callback function
+    */
+    void StartAudio(AudioHandle::InterleavingAudioCallback cb);
+
+    /** Starts the callback
+    \param cb multichannel callback function
+    */
+    void StartAudio(AudioHandle::AudioCallback cb);
+
+    /** Stops the audio if it is running. */
+    void StopAudio();
+
+    /**
+       Switch callback functions
+       \param cb New interleaved callback function.
+    */
+    void ChangeAudioCallback(AudioHandle::InterleavingAudioCallback cb);
+
+    /**
+       Switch callback functions
+       \param cb New multichannel callback function.
+    */
+    void ChangeAudioCallback(AudioHandle::AudioCallback cb);
+
+    /** Updates the Audio Sample Rate, and reinitializes.
+     ** Audio must be stopped for this to work.
+     */
+    void SetAudioSampleRate(SaiHandle::Config::SampleRate samplerate);
+
+    /** Returns the audio sample rate in Hz as a floating point number.
+     */
+    float AudioSampleRate();
+
+    /** Sets the number of samples processed per channel by the audio callback.
+     */
+    void SetAudioBlockSize(size_t blocksize);
+
+    /** Returns the number of samples per channel in a block of audio. */
+    size_t AudioBlockSize();
+
+    /** Returns the rate in Hz that the Audio callback is called */
+    float AudioCallbackRate();
+
+    /** Starts Transfering data from the ADC */
+    void StartAdc();
+
+    /** Stops Transfering data from the ADC */
+    void StopAdc();
+
+    /** Processes the ADC inputs, updating their values */
+    void ProcessAnalogControls(CntrlFreq freq);
+
+    /** Process tactile switches and keyboard states */
+    void ProcessGates();
+
+    /** Process MCP23017 expander*/
+    //void ProcessMCP23017();
+
+    //void DebounceMCP23017();
+
+    /** Process Analog and Digital Controls */
+    inline void ProcessAllControls()
+    {
+        ProcessAnalogControls(Fast);
+        ProcessAnalogControls(Slow);
+        //ProcessGates();
+        //ProcessMCP23017();
+    };
+
+    /** Returns the knob's value
+        \param idx The knob of interest.
+    */
+    float GetKnobValue(Pots idx);
+
+    /** Returns the CV input's value
+        \param idx The CV input of interest.
+    */
+    float GetCvValue(CVs idx);
+
+    //returns state of digital input pin on MCP23017
+    bool GetRawPinState(DigitalInputs idx);
+
+     /** Getter for knob objects
+        \param idx The knob input of interest.
+    */
+    AnalogControl* GetKnob(size_t idx);
+
+    /** Getter for CV objects.
+        \param idx The CV input of interest.
+    */
+    AnalogControl* GetCv(size_t idx);
+
+    /** Print formatted debug log message
+     */
+    template <typename... VA>
+    static void Print(const char* format, VA... va)
+    {
+        Log::Print(format, va...);
+    }
+
+    /** Print formatted debug log message with automatic line termination
+    */
+    template <typename... VA>
+    static void PrintLine(const char* format, VA... va)
+    {
+        Log::PrintLine(format, va...);
+    }
+
+    /** Start the logging session. Optionally wait for terminal connection before proceeding.
+    */
+    static void StartLog(bool wait_for_pc = false)
+    {
+        Log::StartLog(wait_for_pc);
+    }
+
+    DaisySeed                     seed;
+    //LedDriverPca9685<4, true>     led_driver;
+    MidiUartHandler               midi;
+    //Mcp23017                      mcp;
+    AnalogControl                 pots[LAST_POT];
+    AnalogControl                 cv[LAST_CV];
+    GateIn                        gates[LAST_GATE];
+    //Switch                        gates[LAST_GATE];                    
+
+    private:
+
+    /** Set all the HID callback rates any time a new callback rate is established */
+    void SetHidUpdateRates();
+    /** Local shorthand for debug log destination
+    */
+    using Log = Logger<LOGGER_INTERNAL>;
+    //void InitMidi();
+    
+};
+
+                     
+} // namespace daisy
+
+#endif
