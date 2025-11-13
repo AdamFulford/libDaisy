6a203660526c0b821a5d84a967b967273833fa83
AdamFulford <fulford.adam@gmail.com>
2025-11-13 23:42:18 +0000

    more CV slew coeff tweaks

--- src/daisy_VENO-ORBIT_v1_0.h
+++ src/daisy_VENO-ORBIT_v1_0.h
@@ -259,17 +259,17 @@ float MuxSlew[LAST_MUX] =
     // LAST_MUX
 };
 
-//CV (Direct ADC) slew coeffs
+//CV (Direct ADC) slew coeffs Bigger is SLOWER
 float CVSlew[LAST_CV] =
 {
-    0.0000005f, //VOct2_CV
-    0.0000005f, //VOct1_CV
+    0.0000001f, //VOct2_CV
+    0.0000001f, //VOct1_CV
     0.000001f, //LAYER1_CV
     0.000001f, //LAYER2_CV
     0.002f, //START1_CV
     0.002f, //START2_CV
-    0.0000005f, //MOD1_CV
-    0.0000005f, //MOD2_CV
+    0.0000001f, //MOD1_CV
+    0.0000001f, //MOD2_CV
 };
 
 bool CV_Invert[LAST_CV]
